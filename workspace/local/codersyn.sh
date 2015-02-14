21cccd6602c946b4557f73127069c0a017148f4c
upfy@qq.com <upfy@qq.com>
2015-02-14 10:29:34 +0800

    完善代码同步脚本

--- workspace/local/codersyn.sh
+++ workspace/local/codersyn.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-DIR=$1
-PREFIX=luofei_
-fswatch -0 $DIR |  while read -d "" i ; do \
-    date +%H:%M:%S && /usr/bin/rsync -iru   --delete -e 'ssh -i ./rsync-key -p 22' $DIR www-data@103.227.76.203:/code/$PREFIX$DIR
+USER=luofei
+PWD=$( pwd | sed "s/\//#/g")
+fswatch -0 . |  while read -d "" file ; do \
+    FILE_PATH=$(echo $file | sed "s/\//#/g")
+    SHORT_FILE_PATH=$(echo $FILE_PATH | sed "s/$PWD//g")
+    DIR=`echo $SHORT_FILE_PATH|awk -F '#' '{print $2}'`
+    #如果是目录
+    if [  -d "$DIR" ]; then  
+        date +%H:%M:%S && /usr/bin/rsync -iru --exclude .git  --delete -e 'ssh -i ./rsync-key -p 22' $DIR/ www-data@103.227.76.203:/code/$USER.$DIR
+    fi  
 done
--- workspace/local/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#遍历当前的一级目录，监听他们
-for line in `find * -maxdepth 0 -type d`; do 
-    nohup ./codersyn.sh $line > /dev/null &
-done
-
